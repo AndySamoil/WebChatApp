bc84ace0081b26ee8b50e2bec9124fbce67fd2fb
Andrew Samoil <59667646+Andy419@users.noreply.github.com>
2022-01-25 22:15:03 -0700

    slowly getting hooks implemented

--- src/components/home.js
+++ src/components/home.js
@@ -4,9 +4,20 @@ import { getAuth, reload } from "firebase/auth";
 import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import { storage } from "../firebase/config";
 import env from "react-dotenv";
+import { firestoreDB } from "../firebase/config";
+import { addDoc, collection, getDocs, doc, onSnapshot, serverTimestamp, query, orderBy, limit } from "firebase/firestore"; 
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
 
 export default function Home() {
     const Curuser = useContext(AuthContext);
+
+    const chatRef = collection(firestoreDB, "chat");
+
+    const q = query(chatRef, orderBy('createdAt'), limit(6));
+
+    const [messages] = useCollectionData(q);
+    // const [values, loading, error] = useCollectionData<T> (query, options);
 
     const [photoURL, setPhotoURL] = useState("");
     const [uploadedPhoto, setUploadedPhoto] = useState(null);
@@ -18,7 +29,9 @@ export default function Home() {
 
     useEffect(() => {
         pri();
+        //chatHandler();
     }, [])
+    
 
     const fileSelectedHandler = event => {
         // this.setState({
@@ -45,22 +58,79 @@ export default function Home() {
     async function pri(){
         try {
             const pathReference = await getDownloadURL(ref(storage, 'ProfileImages/'+ String(Curuser.user.uid)));
-            console.log(pathReference);
             setPhotoURL(pathReference);
+            console.log("image set");
+            chatHandler();
         } catch (e) {
-            console.log(e)
+            console.log(e);
+            console.log("image not set");
         }
 
     }
 
     function addMessage(e) {
-        setState([...state, {name:message, photo:photoURL}]);
+        CreateNewChat(e);
+        //setState([...state, {name:message, photo:photoURL}]);
+        setMessage("");
     }
 
     const checkenter = (e) => {
         if (e.key == 'Enter') {
+            console.log(e.item);
             addMessage(e);
         }
+    }
+
+    async function chatHandler() {
+        //const auth = getAuth();    
+    
+        //const db = getFirestore();
+        const querySnapshot = await getDocs(collection(firestoreDB, "chat"));
+        //const q = querySnapshot.orderBy('createdAt')
+        console.log(querySnapshot.docs[0].data());
+        
+        //const [curs] = useCollectionData(querySnapshot, { idField: 'id'});
+
+        const arr = [];
+        querySnapshot.forEach((doc) => {
+        //console.log(doc.data());
+        arr.push(doc.data())
+        });
+        console.log(arr)
+        setState(arr);
+
+
+    }
+
+    const CreateNewChat = async (event) => {
+        event.preventDefault();
+        console.log("some Success");
+        try {
+          const auth = getAuth();    
+    
+          //const db = getFirestore();
+          const chatCol = collection(firestoreDB, "chat");
+    
+          //console.log(usersCol);
+          await addDoc(chatCol, {
+            userId: auth.currentUser.uid,
+            message: message,
+            createdAt: serverTimestamp(),
+            PhotoURL: photoURL
+          });
+          console.log("added New Chat");
+          console.log(message);
+    
+    
+    
+        } catch (error) {
+          console.log(error);
+
+        }
+      }
+
+    const handleuserinput = (e) => {
+        setMessage(e.target.value);
     }
 
     return (
@@ -72,21 +142,22 @@ export default function Home() {
             <button onClick={fileUploadHandler}> Upload </button>
             <input type="file" onChange={fileSelectedHandler}/>
             {/* {Curuser.user.uid} <br/> */}
-            {/* <img src = /> */}
+            {/* <img src = />  onChange={({ target }) => setMessage(target.value)}*/}
             <div>
                 <ul>
-                    {state.map(item => {
+                    {messages && messages.map(item => {
                     return(
                         <div>
-                        <p key={item.name}>{item.name}</p>
-                        <img src={item.photo} alt="joe" width="100" height="100"/>
+                        <p key={item.message}>{item.message}</p>
+                        <img src={item.PhotoURL} alt="joe" width="100" height="100"/>
                         </div>
                         );
                     })}
                 </ul>
             </div>
-            <input type="text" onKeyPress={(e) => checkenter(e)} onChange={({ target }) => setMessage(target.value)}/>
+            <input type="text" value={message} onKeyPress={(e) => checkenter(e)} onChange={handleuserinput}/>
             <button onClick={addMessage}>Send</button>
+            <button onClick={chatHandler}>Chats</button>
 
 
         </div>
